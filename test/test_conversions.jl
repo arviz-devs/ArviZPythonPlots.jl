330b595b90f704285781912657effe5c1765935a
Seth Axen <seth@sethaxen.com>
2023-07-27 23:21:15 +0200

    Update conversion tests

--- test/test_conversions.jl
+++ test/test_conversions.jl
@@ -1,9 +1,13 @@
+using ArviZPyPlot
+using ArviZExamples
+using Test
+
 @testset "conversions" begin
     @testset "PSISLOOResult" begin
         idata = load_example_data("centered_eight")
         loo_result = loo(idata; reff=1)
-        loo_py_result = ArviZ.arviz.loo(idata; pointwise=true, reff=1)
-        py_loo_result = ArviZ.topandas(Val(:ELPDData), loo_result)
+        loo_py_result = ArviZPyPlot.arviz.loo(idata; pointwise=true, reff=1)
+        py_loo_result = ArviZPyPlot.topandas(Val(:ELPDData), loo_result)
         @test all(py_loo_result.keys() == loo_py_result.keys())
         @test py_loo_result.elpd_loo ≈ loo_py_result.elpd_loo rtol = 1e-3
         @test py_loo_result.se ≈ loo_py_result.se rtol = 1e-1
@@ -14,8 +18,8 @@
     @testset "WAICResult" begin
         idata = load_example_data("centered_eight")
         waic_result = waic(idata)
-        waic_py_result = ArviZ.arviz.waic(idata; pointwise=true)
-        py_waic_result = ArviZ.topandas(Val(:ELPDData), waic_result)
+        waic_py_result = ArviZPyPlot.arviz.waic(idata; pointwise=true)
+        py_waic_result = ArviZPyPlot.topandas(Val(:ELPDData), waic_result)
         @test all(py_waic_result.keys() == waic_py_result.keys())
         @test py_waic_result.elpd_waic ≈ waic_py_result.elpd_waic rtol = 1e-3
         @test py_waic_result.se ≈ waic_py_result.se rtol = 1e-1
