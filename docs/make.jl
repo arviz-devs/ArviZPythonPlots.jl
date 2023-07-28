53bbfe6dc2fbe39f8e885e96e84652920ac588d2
Seth Axen <seth@sethaxen.com>
2023-07-28 11:42:42 +0200

    Update docs deps and make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,57 +1,8 @@
-using Documenter, Downloads, ArviZ
-using MCMCChains: MCMCChains # make `from_mcmcchains` available for API docs
-using SampleChains: SampleChains # make `from_samplechains` available for API docs
-using PlutoStaticHTML: PlutoStaticHTML
-
-const DOCS_SRC_PATH = joinpath(@__DIR__, "src")
-
-# generate markdown from Pluto notebooks
-output_format = PlutoStaticHTML.documenter_output
-build_opts = PlutoStaticHTML.BuildOptions(
-    DOCS_SRC_PATH;
-    previous_dir=DOCS_SRC_PATH,
-    output_format=output_format,
-    add_documenter_css=false,
-)
-PlutoStaticHTML.build_notebooks(build_opts)
-
-const ASSETS_DIR = joinpath(@__DIR__, "src", "assets")
-const ARVIZ_ASSETS_URL = "https://raw.githubusercontent.com/arviz-devs/arviz-project/main/arviz_logos"
-
-function download_asset(remote_fn, fn=remote_fn)
-    mkpath(ASSETS_DIR)
-    return Downloads.download(
-        joinpath(ARVIZ_ASSETS_URL, remote_fn), joinpath(ASSETS_DIR, fn); verbose=true
-    )
-end
-
-# download arviz-devs org logo assets
-download_asset("ArviZ.png", "logo.png")
-download_asset("ArviZ_white.png", "logo-dark.png")
-download_asset("favicon.ico")
-
-DocMeta.setdocmeta!(
-    ArviZ.MCMCDiagnosticTools, :DocTestSetup, :(using ArviZ.MCMCDiagnosticTools);
-)
-DocMeta.setdocmeta!(ArviZ.InferenceObjects, :DocTestSetup, :(using ArviZ.InferenceObjects);)
-
-doctestfilters = [
-    r"\s+\"created_at\" => .*",  # ignore timestamps in doctests
-]
-
-modules = [ArviZ, InferenceObjects, MCMCDiagnosticTools, PSIS]
-if isdefined(Base, :get_extension)
-    # using Requires, these docstrings are automatically loaded, but as an extension we need
-    # to manually specify the module
-    push!(
-        modules,
-        Base.get_extension(InferenceObjects, :InferenceObjectsMCMCDiagnosticToolsExt),
-    )
-end
+using ArviZPyPlot, Documenter
 
 makedocs(;
-    modules,
-    sitename="ArviZ.jl",
+    modules=[ArviZPyPlot],
+    sitename="ArviZPyPlot.jl",
     pages=[
         "Home" => "index.md",
         "Example Gallery" => ["Matplotlib" => "mpl_examples.md"],
@@ -63,13 +14,9 @@ makedocs(;
     checkdocs=:exports,
     format=Documenter.HTML(;
         prettyurls=haskey(ENV, "CI"),
-        assets=["assets/favicon.ico", "assets/custom.css"],
         sidebar_sitename=false,
         canonical="stable",
     ),
-    doctestfilters,
     linkcheck=true,
-    analytics="G-W1G68W77YV",
 )
 
-deploydocs(; repo="github.com/arviz-devs/ArviZ.jl.git", devbranch="main", push_preview=true)
