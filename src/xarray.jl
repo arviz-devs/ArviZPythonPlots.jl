1054cc2e31948ce7732ca4a1de98d601c1b4f859
Seth Axen <seth@sethaxen.com>
2023-08-09 10:57:12 +0200

    Use topytype utility

--- src/xarray.jl
+++ src/xarray.jl
@@ -2,30 +2,30 @@ PythonCall.Py(data::Dataset) = _to_xarray(data)
 
 function PythonCall.Py(data::InferenceData)
     groups = NamedTuple(data)
-    return arviz.InferenceData(; map(Py, groups)...)
+    return arviz.InferenceData(; map(topytype, groups)...)
 end
 
 function _to_xarray(data::DimensionalData.AbstractDimStack)
-    data_vars = Iterators.map(pairs(DimensionalData.layers(data))) do (k, v)
-        pystr(k) => _to_xarray(v)
-    end |> pydict
-    attrs = pydict(pairs(DimensionalData.metadata(data)))
-    return xarray.Dataset(data_vars; attrs)
+    data_vars = map(_to_xarray, DimensionalData.layers(data))
+    attrs = pairs(DimensionalData.metadata(data))
+    return xarray.Dataset(topytype(data_vars); attrs=topytype(attrs))
 end
 
 function _to_xarray(data::DimensionalData.AbstractDimArray)
-    var_name = pystr(DimensionalData.name(data))
+    var_name = DimensionalData.name(data)
     data_dims = DimensionalData.dims(data)
-    dims = pylist(map(pystr, DimensionalData.name(data_dims)))
-    coords = pydict(Dict(zip(dims, map(pylist, DimensionalData.index(data_dims)))))
-    default_dims = pylist()
+    dims = DimensionalData.name(data_dims)
+    coords = Dict(zip(dims, DimensionalData.index(data_dims)))
+    default_dims = ()
     values = parent(data)
     if Missing <: eltype(values)
         # passing `missing` to Python causes the array to have a `PythonCall.jlwrap` dtype
         values = replace(values, missing => NaN)
     end
-    metadata = pydict(DimensionalData.metadata(data))
-    return da = arviz.numpy_to_data_array(values; var_name, dims, coords, default_dims)
+    metadata = pairs(DimensionalData.metadata(data))
+    kwargs = (; var_name, dims, coords, default_dims)
+    pykwargs = map(topytype, kwargs)
+    return da = arviz.numpy_to_data_array(topytype(values); pykwargs...)
     # if !isempty(metadata)
     #    da.attrs = metadata
     # end
